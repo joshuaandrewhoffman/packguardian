0f36703eaa39178584835fd688f9f0125b2831e7
Josh Hoffman <josh@fitabase.com>
2018-06-18 22:23:55 -0700

    add react custom scrollbar, leaving it default for now, the thinner scrollbars are already a huge improvement. May want to make them slightly more visible down the road

--- src/routes/GridDraft/components/Picks.js
+++ src/routes/GridDraft/components/Picks.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Picks.scss";
+import {Scrollbars} from 'react-custom-scrollbars';
 
 export const Picks = props => {
   //if we can pass an isMobile here, we can do a conditional ? picks-mobile-full : picks
   //this will let us get height: 100% for mobile ux
   return (
-    <div className={"picks"}>
-      {props.pickedCards.map(function(card) {
-        return (
-          <div key={"pick " + card.name + " number " + card.number}><img className={"pick"} alt={card.name} src={card.imageUrl} /></div>
-        );
-      })}
-    </div>
+      <Scrollbars style={{width:240}}>
+        {props.pickedCards.map(function(card) {
+          return (
+            <div key={"pick " + card.name + " number " + card.number}><img className={"pick"} alt={card.name} src={card.imageUrl} /></div>
+          );
+        })}
+          </Scrollbars>
+
   );
 };
 Picks.propTypes = {
